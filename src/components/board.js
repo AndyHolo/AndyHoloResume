345474d2c150e8b6a4ce3358589228bae72602f8
AndyHolo <tangjunanskd@hotmail.com>
2019-02-27 18:14:55 +0800

    update

--- src/components/board.js
+++ src/components/board.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './board.scss';
 
 export class Board extends React.Component {
+    toLeft(){
+        console.log('toLeft')
+    }
+
     render() {
         return (
             <div className="board">
-                <div className="left-arrow"></div>
+                <div className="left-arrow" onClick={this.toLeft}></div>
                 <div className="content"></div>
                 <div className="right-arrow"></div>
             </div>
