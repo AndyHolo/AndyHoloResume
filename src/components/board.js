788360fdbbed46d434e4e67eca52ece091199a45
andyholo <tangjunanskd@hotmail.com>
2019-03-07 17:21:15 +0800

    update

--- src/components/board.js
+++ src/components/board.js
@@ -11,6 +11,9 @@ import cssLogo from '../assets/css.svg';
 import javascriptLogo from '../assets/javascript.svg';
 import reactLogo from '../logo.svg';
 import angularLogo from '../assets/angular.svg';
+import javaLogo from '../assets/java.svg';
+import {IoMdPulse} from 'react-icons/io';
+import springLogo from '../assets/spring.svg'
 
 export class Board extends React.Component {
     constructor(props) {
@@ -41,26 +44,53 @@ export class Board extends React.Component {
     }
 
     render() {
+        let contentTitle;
+        let content;
+        switch (this.state.index) {
+            case (0): {
+                contentTitle = <TitleBoard content="Web development skills"><IoMdGlobe></IoMdGlobe></TitleBoard>;
+                content = (<div className="content-body-fa">
+                    <div className="content-body">
+                        <RoundSkillBar roundSize="8" logoSrc={htmlLogo} title="Html"
+                            barWidth="14" skillValue="70" color="rgb(241, 101, 41)"></RoundSkillBar>
+                        <RoundSkillBar roundSize="8" logoSrc={cssLogo} title="CSS"
+                            barWidth="14" skillValue="60" color="rgb(41, 101, 241)"></RoundSkillBar>
+                        <RoundSkillBar roundSize="8" logoSrc={javascriptLogo} title="JavaScript"
+                            barWidth="14" skillValue="75" color="rgb(247, 223, 30)"></RoundSkillBar>
+                        <RoundSkillBar roundSize="8" logoSrc={reactLogo} title="ReactJS"
+                            barWidth="14" skillValue="50" color="#61dafb"></RoundSkillBar>
+                        <RoundSkillBar roundSize="8" logoSrc={angularLogo} title="Angular"
+                            barWidth="14" skillValue="90" color="rgb(218, 56, 56)"></RoundSkillBar>
+                    </div>
+                </div>);
+                break;
+            }
+            case (1): {
+                contentTitle = <TitleBoard content="Backend development skills"><IoMdPulse></IoMdPulse></TitleBoard>;
+                content = (<div className="content-body-fa">
+                    <div className="content-body">
+                        <RoundSkillBar roundSize="8" logoSrc={javaLogo} title="Java"
+                            barWidth="14" skillValue="75" color="rgb(241, 101, 41)"></RoundSkillBar>
+                        <RoundSkillBar roundSize="8" logoSrc={springLogo} title="Spring"
+                            barWidth="14" skillValue="50" color="rgb(41, 101, 241)"></RoundSkillBar>
+                        <RoundSkillBar roundSize="8" logoSrc={javascriptLogo} title="JavaScript"
+                            barWidth="14" skillValue="75" color="rgb(247, 223, 30)"></RoundSkillBar>
+                        <RoundSkillBar roundSize="8" logoSrc={reactLogo} title="ReactJS"
+                            barWidth="14" skillValue="50" color="#61dafb"></RoundSkillBar>
+                        <RoundSkillBar roundSize="8" logoSrc={angularLogo} title="Angular"
+                            barWidth="14" skillValue="90" color="rgb(218, 56, 56)"></RoundSkillBar>
+                    </div>
+                </div>);
+                break;
+            }
+        }
         return (
             <div>
                 <div className="board" style={this.boardStyle}>
                     <div className="left-arrow" onClick={this.toLeft.bind(this)}></div>
                     <div className="content">
-                        <TitleBoard content="Web development skills"><IoMdGlobe></IoMdGlobe></TitleBoard>
-                        <div className="content-body-fa">
-                            <div className="content-body">
-                                <RoundSkillBar roundSize="8" logoSrc={htmlLogo} title="Html"
-                                    barWidth="14" skillValue="70" color="rgb(241, 101, 41)"></RoundSkillBar>
-                                <RoundSkillBar roundSize="8" logoSrc={cssLogo} title="CSS"
-                                    barWidth="14" skillValue="60" color="rgb(41, 101, 241)"></RoundSkillBar>
-                                <RoundSkillBar roundSize="8" logoSrc={javascriptLogo} title="JavaScript"
-                                    barWidth="14" skillValue="75" color="rgb(247, 223, 30)"></RoundSkillBar>
-                                <RoundSkillBar roundSize="8" logoSrc={reactLogo} title="ReactJS"
-                                    barWidth="14" skillValue="50" color="#61dafb"></RoundSkillBar>
-                                <RoundSkillBar roundSize="8" logoSrc={angularLogo} title="Angular"
-                                    barWidth="14" skillValue="90" color="rgb(218, 56, 56)"></RoundSkillBar>
-                            </div>
-                        </div>
+                        {contentTitle}
+                        {content}
                     </div>
                     <div className="right-arrow" onClick={this.toRight.bind(this)}></div>
                 </div>
